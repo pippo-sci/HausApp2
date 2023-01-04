e6ec3a736b94bbe1272d69ea89011f23a9413a42
Felipe Ramos <framosmorale@student.unimelb.edu.au>
2023-01-04 17:07:08 +1100

    preprocess data dump

--- check.py
+++ check.py
@@ -1,43 +1,87 @@
 import pandas as pd
-import dash
-import dash_core_components as dcc
-import dash_hatml_components as html
-from dash.dependencies import Input, Output
+import geopandas as gpd
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
-import plotly.graph_objs as go
+import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__)
+#import plotly.graph_objs as go
 
-df = pd.read_csv('melb.csv')
+app = Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP],
+        meta_tags = [{'name':'viewport', 'content': 'width=device-width'}])
 
-app.layout = html.Div([
-    html.H1('HausApp', style= {'text-align':'center'}),
-    dcc.Dropdown(id='select_year',
-     options=[{'label':'', 'value': }],
-     multi=False,
-     value=,
-     style={'width':'40%'}),
-    html.Div(id='output_container', children =[]),
-    html.Br(),
-    htmldcc.Graph(id='map',figure={})
+df = gpd.read_file('melb.geojson')
 
+app.layout = dbc.Container([
+    dbc.NavbarSimple(className= "color4", children=[], brand="Initial",
+        brand_href="#",
+        color="primary",
+        dark=True),
+    html.Div(className="parallax container-fluid", children=[
+        html.H1('HausApp', className= "text-center align-middle text"), 
+        html.Div(className="text-center align-middle text2", children=[
+            html.P("Living in Sydney for a couple of months, I found isn't easy look a flatshare"),
+            html.P("for couples as most search websites are oriented to singles."),
+            html.P("I hope this dashboard help you...despite is still a work in process")])
+    ]),
+    html.Div(className="color5", children=[
+        html.Div(className= "row container-fluid align-middle clearfix", children= [
+            dcc.Dropdown(id='select_year',
+                options=[{'label':'1', 'value': 1},{'label':'2', 'value': 2}],
+                multi=False,
+                value=1,
+                style={'width':'40%'})
+        ])]),
 
+    dcc.Graph(id='map',figure={}),
+
+    dbc.Container([
+        dbc.Row([
+            dbc.Col(className="color3", children=[
+                html.Div(""),
+                dcc.Graph()
+            ]),
+            dbc.Col(className="color4",children=[
+                html.Div(""),
+                dcc.Graph()
+            ]),
+            dbc.Col(className="color2",children=[
+                html.Div("Cheapest neighborhoods"),
+                dcc.Graph()
+            ])
+        ]),
+        dbc.Row([
+            dbc.Col(className="color2",children=[
+                html.Div(""),
+                dcc.Graph()
+            ]),
+            dbc.Col(className="color1",children=[
+                html.Div(className="color3",children=""),
+                dcc.Graph()
+            ]),
+            dbc.Col(className="color3",children=[
+                html.Div("Most expensive neighborhoods"),
+                dcc.Graph()
+            ])
+        ])
+    ]),
+    html.Div(id='output_container', children =[])
 ])
 
 
 # ----
 
 @app.callback(
-    [Output(component_id='output-container', component_property='children'), Output(component_id='map', component_property='figure')],
+    [Output(component_id='output_container', component_property='children'), Output(component_id='map', component_property='figure')],
     [Input(component_id='select_year', component_property='value')]
 )
 
 def update_graph(selected_option):
     container = 'Selected year is {}'.format(selected_option)
 
-    dff = df.copy()
+    tt = df.copy()
 
-    fig = px.choropleth
+    fig = px.choropleth_mapbox(tt, geojson= tt.geometry, locations= tt.index, color=tt['mean'],
+                           mapbox_style="stamen-toner", hover_name = 'neigh', hover_data=['count'], opacity = 0.6, center= {'lat':-37.8097305,'lon':144.9039946})
 
     return container, fig
 
--- readme.md
+++ readme.md
@@ -3,3 +3,24 @@
 Web app to help you find a renting place in Sydney and Melbourne, Australia
 
 This is a rebuild of HausApp. After Heroku shutdown their free tier I decided to recreate the proyect.
+The original stack used on the webapp parte Bokeh for the graphs, Flask for the backend and Boostrap for the frontend
+
+
+## Architecture
+
+The project use multiple free to use tools on the cloud for each architecural component
+
+#### Data extraction
+Scrapy for data scrapping and Zyte for scheduling jobs
+
+#### Backend
+Container runs Python script to clean and summaries data and store it on Firebase
+
+#### Webapp
+Frontend Build on Dash (Flask + React + Plotly)
+It also replaces a scheduling server, by creating a number of queries to check update the data and run mantainability scripts
+
+#### Monitoning
+
+
+## Tech stack
